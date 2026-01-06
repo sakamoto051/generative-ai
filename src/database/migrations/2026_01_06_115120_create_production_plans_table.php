a458639dac4b4354afbe927b9832640d71f1dc63
t-sakamoto <sakamoto051eng@gmail.com>
2026-01-06 20:54:22 +0900

    feat(db): Create migrations for production_plans and production_plan_details

---
+++ src/database/migrations/2026_01_06_115120_create_production_plans_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('production_plans', function (Blueprint $table) {
+            $table->id();
+            $table->string('plan_code')->unique();
+            $table->string('name');
+            $table->date('start_date');
+            $table->date('end_date');
+            $table->string('status')->default('Draft');
+            $table->foreignId('created_by')->constrained('users');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('production_plans');
+    }
+};
